8f52ecf058f8ba204b4862a38cbc0a6b4fa619c9
tuliocl <tcl@ic.ufal.br>
2024-04-28 18:05:59 -0300

    Inicia implementação do spring security

--- src/main/java/tulio/ecommerce/User/UserModel.java
+++ src/main/java/tulio/ecommerce/User/UserModel.java
@@ -6,11 +6,17 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.Data;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Data
 @Entity(name = "TB_User")
-public class UserModel {
+public class UserModel implements UserDetails {
     @Id
     @GeneratedValue(generator = "UUID")
     UUID id;
@@ -24,4 +30,43 @@ public class UserModel {
     
     @Column(columnDefinition = "double default 0.0")
     double saldo;
+
+    UserRole role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if(this.role == UserRole.ADMIN)
+        {
+            return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"),new SimpleGrantedAuthority("ROLE_USER"));
+        }
+
+        return  List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getUsername() {
+   
+        return login;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
